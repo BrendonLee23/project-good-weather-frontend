1dd9c37999253ba8b94a3fc5a5cee27daf7993db
Brendo <brendow_prime@hotmail.com>
2023-11-26 16:56:21 -0400

    Feat/ folders structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,15 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import axios from 'axios'
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage/HomePage"
+
 
 function App() {
-  const [data, setData] = useState([])
-  const MyShal = withReactContent(Swal)
-  console.log(data)
-
-  const array = [
-    { title: 'Ola', day: 'hoje', user: 'brendo' },
-  ]
-
-  useEffect(() => {
-    loadDataFromAPI()
-  }, [])
-
-  function loadDataFromAPI() {
-    const apiURL = import.meta.env.VITE_APIURL;
-    axios
-      .get(`${apiURL}/todosa`)
-      .then(({ dataerror }) => {
-        setData(data);
-      })
-      .catch(() => {
-        MyShal.fire({
-          title: "Ooops!",
-          text: "Something went wrong... 😢"
-        })
-      });
-  }
-
 
   return (
-    <>
-      <div>
-        <h1>{array.title}</h1>
-        <h2>{array.day}</h2>
-        <h2>{array.user}</h2>
-      </div>
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage/>}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/style/GlobalStyle.jsx
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from 'syled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        user-select: none;
+        box-sizing: border-box;
+    }
+`
