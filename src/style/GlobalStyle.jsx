778abff2b3f7df6e6c4c7f3a5b7b4c599dfe7734
Brendo <brendow_prime@hotmail.com>
2023-11-26 20:36:41 -0400

    Feat/ HomePage > LeftInfos: Static Version

--- src/style/GlobalStyle.jsx
+++
@@ -1,8 +0,0 @@
-import { createGlobalStyle } from 'syled-components';
-
-export const GlobalStyle = createGlobalStyle`
-    * {
-        user-select: none;
-        box-sizing: border-box;
-    }
-`
---
+++ src/styles/GlobalStyle.jsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+
+export const GlobalStyle = createGlobalStyle`
+    * {
+        user-select: none;
+        box-sizing: border-box;
+        font-family: 'Montserrat', sans-serif;
+        font-family: 'Poppins', sans-serif;
+    }
+    ::placeholder{
+            color: black;
+        }
+
+`
