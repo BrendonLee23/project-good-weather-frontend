354de12ff33f77786ce504a90a01d32401b77b1e
Brendo <brendow_prime@hotmail.com>
2023-11-27 01:55:10 -0400

    Feat/ RightInfos: Static Version

--- src/components/RightInfos/RightInfos.jsx
+++ src/components/RightInfos/RightInfos.jsx
@@ -17,10 +17,46 @@ export default function RightInfos() {
                 <h1 onClick={() => handleTitleClick('today')}>Hoje</h1>
                 <h2 onClick={() => handleTitleClick('nextDays')}>Próximos dias</h2>
             </TitleOptions>
+            <CityAndCoordinates>
+                <h1>São Paulo</h1>
+                <p>Lat:  44.34  <span>...</span>  Long: 10.99</p>
+            </CityAndCoordinates>
             {selectedComponent === 'today' ? <TodayInfos /> : <NextDayInfos />}
+            <Footer>
+                <p>Dados fornecidos pela <span>Open Weather API</span></p>
+            </Footer>
         </RightInfosContainer>
     )
 }
+
+const CityAndCoordinates = styled.div`
+    display:flex;
+    flex-direction: column;
+
+    h1{
+        font-family: Poppins;
+        font-size: 100px;
+        font-weight: 400;
+        line-height: 48px;
+        letter-spacing: 0em;
+        text-align: left;
+        color: #222222;
+    }
+    p{
+        font-family: Poppins;
+        font-size: 20px;
+        font-weight: 400;
+        line-height: 48px;
+        letter-spacing: 0em;
+        text-align: left;
+        color: #222222;
+        margin-top: 20px;
+        margin-left: 5px;
+    }
+    span{
+        color:#D8D8D8;
+    }
+`
 
 const RightInfosContainer = styled.div`
     background-color:#D8D8D8;
@@ -40,6 +76,7 @@ const TitleOptions = styled.div`
     align-items: center;
     justify-content: start;
     gap:50px;
+    margin-bottom: 40px;
     h1{
         color: ${(props) =>
         props.selectedComponent === 'today' ? 'black' : '#a7a7a7'};
@@ -62,4 +99,20 @@ const TitleOptions = styled.div`
         text-align: left;
         cursor: pointer;
     }
+`
+const Footer = styled.div`
+    position: absolute;
+    bottom: 0;
+    margin-bottom: 20px;
+    p{
+        font-family: Poppins;
+        font-size: 18px;
+        font-weight: 400;
+        line-height: 48px;
+        letter-spacing: 0em;
+        text-align: left;
+    span{
+        color: #96A7F2;
+    }
+}
 `
